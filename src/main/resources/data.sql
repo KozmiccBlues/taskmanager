5be321f8681ff7d0f41aa1308acf4c8754852beb
Ola <kozlowska.aleksandra92@gmail.com>
2022-05-03 19:32:17 +0200

    Błąd przy tworzeniu tabeli i wgraniu do bazy danych h2. rror starting ApplicationContext. To display the conditions report re-run your application with 'debug' enabled. 2022-05-03 19:31:07.959 ERROR 13004 --- [  restartedMain] o.s.boot.SpringApplication               : Application run failed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO task(id, name, description) VALUES (1, 'Umyć okno', 'Mycie szyby i ramy płynem do szyb');
+INSERT INTO task(id, name, description) VALUES (2, 'Wytrzeć kurze', 'Wytrzeć kurze Pronto');
+
+
